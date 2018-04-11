3eec6cfbb5964e53591cb849daf6f6a2e1831bee
Shane Talbert <stalbert1@tampabay.rr.com>
2018-04-11 19:50:32 -0400

    initial commit

--- Hello_Github/ViewController.swift
+++ Hello_Github/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblMyName: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  
+    @IBAction func sayMyNamePressed(_ sender: UIButton) {
+        
+        lblMyName.text = "Shane"
     }
-
+    
 
 }
 
