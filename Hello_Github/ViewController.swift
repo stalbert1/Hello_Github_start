22d244ff5c17b8325f0df635954ee5874ac26ed5
Shane Talbert <stalbert1@tampabay.rr.com>
2018-04-11 20:14:12 -0400

    changed my name and set constraints

--- Hello_Github/ViewController.swift
+++ Hello_Github/ViewController.swift
@@ -13,15 +13,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblMyName: UILabel!
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        lblMyName.text = "Starting val"
     }
 
   
     @IBAction func sayMyNamePressed(_ sender: UIButton) {
         
         lblMyName.text = "Shane"
+        
+        
     }
     
 
